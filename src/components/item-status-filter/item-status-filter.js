974a68f2cf0d8fd4133c464401608b0d1ecc3047
R-Mosolov <r.v.mosolov@ya.ru>
2019-07-21 19:56:49 +0300

    Revert 2.0 – Lesson 41 is done

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './item-status-filter.css';
 
-<<<<<<< HEAD
 export default class ItemStatusFilter extends Component {
     render() {
         return (
@@ -17,19 +16,3 @@ export default class ItemStatusFilter extends Component {
         );
     }
 }
-=======
-const ItemStatusFilter = () => {
-  return (
-    <div className="btn-group">
-      <button type="button"
-              className="btn btn-info">All</button>
-      <button type="button"
-              className="btn btn-outline-secondary">Active</button>
-      <button type="button"
-              className="btn btn-outline-secondary">Done</button>
-    </div>
-  );
-};
-
-export default ItemStatusFilter;
->>>>>>> parent of 12955b88... Lesson 34 is done
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './todo-list-item.css';
 
-<<<<<<< HEAD
 export default class TodoListItem extends Component {
 
     state = {
@@ -62,34 +61,3 @@ export default class TodoListItem extends Component {
         );
     };
 }
-=======
-const TodoListItem = ({ label, important = false }) => {
-
-  const style = {
-    color: important ? 'steelblue' : 'black',
-    fontWeight: important ? 'bold' : 'normal'
-  };
-
-  return (
-    <span className="todo-list-item">
-      <span
-        className="todo-list-item-label"
-        style={style}>
-        {label}
-      </span>
-
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
-
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
-  );
-};
-
-export default TodoListItem;
->>>>>>> parent of 12955b88... Lesson 34 is done
